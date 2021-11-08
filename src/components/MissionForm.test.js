fd02121523a989e6831120ade2e55bbbc0358b83
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-08 13:25:38 -0800

    set test file - MissionForm.test.js and render a test to test test file ok

---
+++ src/components/MissionForm.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import MissionForm from './MissionForm';
+
+test("renders without errors", ()=> {
+    render(<MissionForm/>);
+});
