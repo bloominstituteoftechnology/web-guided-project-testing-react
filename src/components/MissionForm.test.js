78b1cea63189873c497786c4ac13988bb140b81d
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-08 15:34:06 -0800

    3. b- Act: Find our button. Press our button. !!! SOMETHING IS WRONG -- error message

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -38,7 +38,11 @@ test ("renders getData message if button is clicked", ()=> {
 
     // Arrange: Render our component with isFetchingData === false
     render(<MissionForm isFetchingData={false} />);
+    
     //Act: Find our button. Press our button.
+    const button = screen.queryByRole("button");
+    userEvent.click(button);
+    
     //Assert: See if a function gets called?
 
 })
