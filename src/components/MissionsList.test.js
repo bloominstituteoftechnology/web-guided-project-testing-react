6d7adbef7b2bb6bfb079373565c207fa6a1bf87a
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-08 18:52:03 -0800

    MissionsList.test.js Arrange 2: render component with 3 missions

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -6,6 +6,21 @@ test ("renders without errors", () => {
     render (<MissionsList missions={[]}/>)
 })
 
+const testMissions = [
+    {
+        mission_id: 1,
+        mission_name: "Mission 1"
+    },
+    {
+        mission_id: 2,
+        mission_name: "Mission 3"
+    },
+    {
+        mission_id: 3,
+        mission_name: "Mission 3"
+    }
+]
+
 test ("renders 3 missions after rendering no missions", () => {
     //Arrange 1: render component with no missions
     const { rerender } = render(<MissionsList missions={[]}/>);
@@ -17,8 +32,10 @@ test ("renders 3 missions after rendering no missions", () => {
     //Asset 1: Check that no missions have been rendered.
     expect(missions).toHaveLength(0);
 
-    console.log('testStuff: ', testStuff);
+    // console.log('testStuff: ', testStuff);
     //Arrange 2: render component with 3 missions   
+    rerender(<MissionsList missions={testMissions}/>);
+
     //Act 2: Find all missions    
     //Asset 2s: Check that no missions have been rendered.  
 })
