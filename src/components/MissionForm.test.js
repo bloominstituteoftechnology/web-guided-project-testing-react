084309638ad7b2d8467536f39b7245598fee780f
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-08 16:29:53 -0800

    3. Assert: See if a function gets called?

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -46,9 +46,11 @@ test ("renders getData message if button is clicked", ()=> {
     userEvent.click(button);
     userEvent.click(button);
     userEvent.click(button);
+    
 
     //Assert: See if a function gets called?
     // console.log('fakeGetData: ', fakeGetData);
     console.log('fakeGetData.mock: ', fakeGetData.mock);
+    expect(fakeGetData.mock.calls.length).toBe(3);
 
 })
