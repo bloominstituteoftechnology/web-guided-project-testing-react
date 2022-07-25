2de1d3c53906adbd6087ca56c7d03eb66a6c52d6
RinatTr <rinatreg@gmail.com>
2022-07-25 13:23:33 -0400

    work in progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -39,4 +39,21 @@ test('renders button when isFetchingData is false', () => {
 })
 
 //does getData execute when user executes behavior (click button)?
+test('calls getData when button is clicked', () => {
+    const mockGetData = jest.fn(); //creates a mock function
+
+    render(<MissionForm isFetchingData={false} getData={mockGetData} />);
+
+    const getDataButton = screen.getByRole("button");
+    userEvent.click(getDataButton);
+    // userEvent.click(getDataButton);
+
+    console.log(mockGetData.mock)
+
+    expect(mockGetData.mock.calls.length).toBe(1);
+    expect(mockGetData.mock.calls).toHaveLength(1);
+    expect(mockGetData).toHaveBeenCalledTimes(1);
+
+})
+//Mocks - fake functions. 
 
