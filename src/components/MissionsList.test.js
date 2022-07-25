d1389beb50a7e667683095e2579afb3cf4da7ddc
RinatTr <rinatreg@gmail.com>
2022-07-25 14:05:24 -0400

    work in progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react'
 import MissionsList from "./MissionsList";
 
+const missions = [
+    {mission_name: 'Thaicom', mission_id: "someId"},
+    {mission_name: 'Telstor', mission_id: "someOtherId"},
+] 
 test('renders without errors', () => {
     render(<MissionsList missions={[]}/>)
 })
@@ -10,6 +14,16 @@ test('renders without errors', () => {
 
 test("MissionsList shows data when rerendered with new missions data", () => {
 //first stage: missions props is an empty array
+const { rerender } = render(<MissionsList missions={[]}/>)
+//rerender() - allows us to render the component every time there are new props.
+
+
+let missionDivs = screen.queryAllByTestId("mission");
+expect(missionDivs).toEqual([]);
 
 //Second stage: missions props is an array with missions
+rerender(<MissionsList missions={missions} />)
+missionDivs = screen.queryAllByTestId("mission");
+expect(missionDivs).toHaveLength(2);
+
 })
